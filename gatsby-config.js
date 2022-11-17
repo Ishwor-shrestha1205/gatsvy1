74f3a6cc9f93358f5fa46b9669469b5ed4dbe8a8
Ishwor-shrestha1205 <shrestha_ishwor@retail-ai.jp>
2022-11-17 15:10:24 +0900

    fix version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,38 @@
+// module.exports = {
+//   siteMetadata: {
+//     title: "My First Gatsby Site",
+//     siteUrl: `https://www.yourdomain.tld`,
+//   },
+//   plugins: [
+//   "gatsby-plugin-image",
+//   "gatsby-plugin-sharp",
+//   {
+//     resolve: "gatsby-source-filesystem",
+//     options: {
+//       name: `blog`,
+//       path: `${__dirname}/blog`,
+//     }
+//   },
+//   "gatsby-plugin-mdx",
+//   "gatsby-transformer-sharp",
+//   ],
+// };
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-  "gatsby-plugin-image",
-  "gatsby-plugin-sharp",
-  {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `${__dirname}/blog`,
-    }
-  },
-  "gatsby-plugin-mdx",
-  "gatsby-transformer-sharp",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
   ],
 };
